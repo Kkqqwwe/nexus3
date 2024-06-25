1791b5346f6a8292ebbf946e386eaa492f2bf77d
Kkqqwwe <154258107+Kkqqwwe@users.noreply.github.com>
2024-06-25 10:35:15 +0000

    DFsa

---
+++ nexus-project/target/riscv32i-unknown-none-elf/debug/nexus-project.d
@@ -0,0 +1,1 @@
+/workspaces/nexus3/nexus-project/target/riscv32i-unknown-none-elf/debug/nexus-project: /workspaces/nexus3/nexus-project/src/main.rs
---
+++ nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project.d
@@ -0,0 +1,1 @@
+/workspaces/nexus3/nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project: /workspaces/nexus3/nexus-project/src/main.rs
